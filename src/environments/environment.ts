2284b4f39e643c4b5f12250ddf97c7c6798b96be
CrayolitoBC <jsahonero@bancosol.com.bo>
2026-08-04 14:46:39 -0400

    cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://casa-color-backend-k14sn1-f7d239-72-60-156-101.sslip.io',
+  apiUrl: 'https://casa-color-backend-k14sn1-f7d239-72-60-156-101.sslip.io',
   /** Deuda: key del clon en texto plano. Restringir por referrer en GCP Console. */
   googleMapsApiKey: 'AIzaSyDP77pKWxWWWITgBm28_Z17uiGCB2ibYXU',
 };
